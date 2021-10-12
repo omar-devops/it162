1c7a51ba5c311e3368ab8829d40dc4701dbdd565
omar-devops <72107576+omar-devops@users.noreply.github.com>
2021-10-11 17:43:40 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer>
+      <p><small>&copy; 2021 by <a href="contact.php">Omurbek Mamasaliev</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </div>
+     
+  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+     
+  <script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+ </script>
+     
+ </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>Omurbek Mamasaliev's IT161 Portal Website</title>
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+ </head>
+ <body>
+     <header>
+     <h1>Omurbek Mamasaliev's IT162 Portal Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.html" class="active">Welcome</a>
+       <a href="aia.html">AIA</a>
+       <a href="big/index.html">BIG</a>
+       <a href="https://drive.google.com/file/d/1vMtOta45c-KgNQZ9AHhLZqViz8xw4A1D/view">Flowchart</a>
+       <a href="http://web-students.net/site34/it161/fp/fp.html">Final Project</a>
+       <a href="contact.php">Contact Omurbek</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+     </nav>
+     </header>
+     
+   <div class="wrapper">
+     
