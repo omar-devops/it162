0e0e97903164af57469721c7989f1f383d76ffe6
omar-devops <72107576+omar-devops@users.noreply.github.com>
2021-10-11 17:45:38 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php';?>
+     <img class="desktop" src="image/desktop.jpg" alt="Riding the horse" />
+       
+     <img class="tablet" src="image/tablet.jpg" alt="Uludag ski resort" />
+       
+     <img class="phone" src="image/phone.jpg" alt="Hiking" />
+       
+       <h2 class="subheader">A little about me!</h2>
+       
+       <p>I am from Kyrgyzstan, and we were nomads. As you can see from the photo I am riding the horse. The horse is associated with our nomadic traditions. Horse riding games is one of our cultural heritage that we preserve through generations.</p>
+       <p>My another hobby is snowbarding, even though I am new, I already fell in love with it. My first experience was in Bursa, Uluadag. It is in Turkey. I recommend this sport for people who love extreme and winter :)</p>
+       <p>Last but not the least, it is hiking. If I am lost you can find me in forests, climbing mountains or doing picnic in parks. Love fauna and flora.</p>
+<?php include 'includes/footer.php';?>
